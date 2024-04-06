ece3518a687a8c24d04d212405ecc32c5be52a74
Cătălin Candea <80335024+CandeaCatalin@users.noreply.github.com>
2024-04-06 18:32:47 +0300

    Refactor analysis service

--- VehiclesFleet.Services/AnalysisService.cs
+++ VehiclesFleet.Services/AnalysisService.cs
@@ -30,75 +30,104 @@ public class AnalysisService : IAnalysisService
 
             var telemetriesForVehicle = await dataContext.VehicleTelemetries.Where(vt => vt.VehicleId == vehicleId)
                 .OrderBy(vt => vt.CreateAt).ToListAsync();
-            var filteredList = telemetriesForVehicle.Where(v => v.ActualSpeed == 0).ToList();
 
-            if (filteredList.Count >= 2)
+            var eligibleTelemetries = GetEligibleTelemetries(telemetriesForVehicle);
+
+            if (eligibleTelemetries.Count() != 0)
             {
-                // Get the last two entities that meet the condition
-
-                var last = filteredList.LastOrDefault();
-                var first = filteredList.ElementAt(filteredList.Count -2);
-                // Find the indices of the last two entities in the original list
-                int lastIndex =
-                    telemetriesForVehicle.IndexOf(last); // Index of the second-to-last entity
-                int secondLastIndex = telemetriesForVehicle.IndexOf(first); // Index of the last entity
-
-                if (lastIndex != -1 && secondLastIndex != -1)
+                var vehicleAnalysis = new VehicleAnalysis
                 {
-                    // Determine the range of indices for entities between the last two entities
-                    int startIndex = Math.Min(lastIndex, secondLastIndex);
-                    int endIndex = Math.Max(lastIndex, secondLastIndex);
-
-                    // Extract all entities between the last two occurrences
-                    var totalEntities = telemetriesForVehicle.Skip(startIndex).Take(endIndex - startIndex +1)
-                        .ToList();
-                    var entitiesBetween = telemetriesForVehicle.Skip(startIndex + 1).Take(endIndex - startIndex - 1)
-                        .ToList();
-                    var minimumSpeed = entitiesBetween.Where(e => e.ActualSpeed != 0).Min(e => e.ActualSpeed);
-                    var maxSpeed = entitiesBetween.Where(e => e.ActualSpeed != 0).Max(e => e.ActualSpeed);
-                    var averageSpeed = entitiesBetween
-                        .Average(e => e.ActualSpeed);
-                    var fuels = totalEntities.Select(x => x.Fuel);
-                    var maxFuel = fuels.ElementAt(0);
-                    var minFuel = maxFuel;
-                    var fuelConsumption = 0.0;
-                    foreach (var f in fuels)
-                    {
-                        if (f > minFuel)
-                        {
-                            fuelConsumption = (double)(maxFuel - minFuel);
-                            maxFuel = f;
-                            minFuel = f;
-                        }
-                        else
-                        {
-                            minFuel = f;
-                        }
-                    }
-
-                    if (fuelConsumption == 0)
-                    {
-                        fuelConsumption = (double)(maxFuel - minFuel);
-                    }
-                    
-                    var vehicleAnalysis = new VehicleAnalysis
-                    {
-                        Id = Guid.NewGuid(),
-                        VehicleId = vehicleId,
-                        UserId = vehicle.UserId.ToString(),
-                        Vehicle = vehicle,
-                        MinimumSpeed = minimumSpeed,
-                        MaximumSpeed = maxSpeed,
-                        AverageSpeed = averageSpeed,
-                        FuelConsumption = fuelConsumption,
-                        HasTierPressureAnomaly = false,
-                        TotalKilometersPassed = telemetriesForVehicle.LastOrDefault()!.KilometersSinceStart
-                    };
-                    await dataContext.VehiclesAnalysis.AddAsync(vehicleAnalysis);
-                    await dataContext.SaveChangesAsync();
-
-                }
+                    Id = Guid.NewGuid(),
+                    VehicleId = vehicleId,
+                    UserId = vehicle.UserId.ToString(),
+                    Vehicle = vehicle,
+                    MinimumSpeed = GetMinimumSpeed(eligibleTelemetries),
+                    MaximumSpeed = GetMaximumSpeed(eligibleTelemetries),
+                    AverageSpeed = GetAverageSpeed(eligibleTelemetries),
+                    FuelConsumption = GetFuelConsumption(eligibleTelemetries),
+                    HasTierPressureAnomaly = ArePressureAnomalies(eligibleTelemetries),
+                    TotalKilometersPassed = telemetriesForVehicle.LastOrDefault()!.KilometersSinceStart
+                };
+                await dataContext.VehiclesAnalysis.AddAsync(vehicleAnalysis);
+                await dataContext.SaveChangesAsync();
             }
         }
+    }
+
+    private IList<VehicleTelemetry> GetEligibleTelemetries(IList<VehicleTelemetry> telemetries)
+    {
+        var filteredList = telemetries.Where(v => v.ActualSpeed == 0).ToList();
+        if (filteredList.Count >= 2)
+        {
+            var last = filteredList.LastOrDefault();
+            var first = filteredList.ElementAt(filteredList.Count - 2);
+         
+            int lastIndex =
+                telemetries.IndexOf(last); 
+            int secondLastIndex = telemetries.IndexOf(first);
+
+            if (lastIndex != -1 && secondLastIndex != -1)
+            {
+            
+                int startIndex = Math.Min(lastIndex, secondLastIndex);
+                int endIndex = Math.Max(lastIndex, secondLastIndex);
+
+                return telemetries.Skip(startIndex).Take(endIndex - startIndex + 1)
+                    .ToList();
+            }
+        }
+
+        return new List<VehicleTelemetry>();
+    }
+    
+    private bool ArePressureAnomalies(IList<VehicleTelemetry> telemetries)
+    {
+        var tirePressure = telemetries.Select(x => x.TirePressure).ToList();
+        return tirePressure.Max() - tirePressure.Min() >= 10;
+    }
+
+    private int GetMinimumSpeed(IList<VehicleTelemetry> telemetries)
+    {
+        return telemetries.Where(e => e.ActualSpeed != 0).Min(e => e.ActualSpeed);
+    }
+    
+    private int GetMaximumSpeed(IList<VehicleTelemetry> telemetries)
+    {
+        return telemetries.Where(e => e.ActualSpeed != 0).Max(e => e.ActualSpeed);
+    }
+
+    private double GetAverageSpeed(IList<VehicleTelemetry> telemetries)
+    {
+        return telemetries.Where(t=>t.ActualSpeed != 0)
+            .Average(e => e.ActualSpeed);
+    }
+    
+    private double GetFuelConsumption(IList<VehicleTelemetry> telemetries)
+    {
+        var fuels = telemetries.Select(x => x.Fuel).ToList();
+                    
+        var maxFuel = fuels.ElementAt(0);
+        var minFuel = maxFuel;
+        var fuelConsumption = 0.0;
+        foreach (var f in fuels)
+        {
+            if (f > minFuel)
+            {
+                fuelConsumption = (double)(maxFuel - minFuel);
+                maxFuel = f;
+                minFuel = f;
+            }
+            else
+            {
+                minFuel = f;
+            }
+        }
+
+        if (fuelConsumption == 0)
+        {
+            fuelConsumption = (double)(maxFuel - minFuel);
+        }
+
+        return fuelConsumption;
     }
 }
